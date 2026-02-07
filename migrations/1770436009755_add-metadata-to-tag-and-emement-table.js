ebd946531b4ff61423a282ab3278352ad8a70b62
Aniket <aniketraj28042003@gmail.com>
2026-02-07 09:20:53 +0530

    feat(db): add metadata column in tag and element table

---
+++ migrations/1770436009755_add-metadata-to-tag-and-emement-table.js
@@ -0,0 +1,30 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+export const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+  pgm.sql(`
+    ALTER TABLE tags
+    ADD COLUMN metadata JSONB;
+  `);
+  pgm.sql(`
+    ALTER TABLE elements
+    ADD COLUMN metadata JSONB;
+  `);
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {
+  pgm.sql(`ALTER TABLE tags DROP COLUMN metadata;`);
+  pgm.sql(`ALTER TABLE elements DROP COLUMN metadata;`);
+};
